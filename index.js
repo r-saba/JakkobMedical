d26f0486e1070e5d97741b7b598e78cbfe394ee3
ragith <ragith.coolkid@gmail.com>
2020-04-23 15:03:04 -0400

    remove debugger

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function createWindow() {
   // and load the index.html of the app.
   win.loadFile(`index.html`);
   // Open the DevTools.
-  win.webContents.openDevTools();
+  //win.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
